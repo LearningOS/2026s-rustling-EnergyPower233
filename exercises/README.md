33fda93d8c832154f7ef19915640a367ed982be2
E_Power <energy_power233@qq.com>
2026-03-16 21:27:30 +0800

    2

--- exercises/README.md
+++ exercises/README.md
@@ -9,9 +9,9 @@
 | vecs            | §8.1         |
 | move_semantics  | §4.1-2       |
 | structs         | §5.1, §5.3   | X   |
-| enums           | §6, §18.3    |
-| strings         | §8.2         |
-| modules         | §7           |
+| enums           | §6, §18.3    | X   |
+| strings         | §8.2         | X   |
+| modules         | §7           | X   |
 | hashmaps        | §8.3         |
 | options         | §10.1        |
 | error_handling  | §9           |
--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -34,6 +33,10 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
+        let team_1_goals = team_1_score;
+        let team_2_goals = team_2_score;
+        update_team_stats(&mut scores,team_1_name,team_1_score,team_2_score);
+        update_team_stats(&mut scores,team_2_name,team_2_score,team_1_score);
         // TODO: Populate the scores table with details extracted from the
         // current line. Keep in mind that goals scored by team_1
         // will be the number of goals conceded from team_2, and similarly
@@ -42,6 +45,24 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
     }
     scores
 }
+fn update_team_stats(
+    scores: &mut HashMap<String, Team>, 
+    team_name: String, 
+    scored: u8, 
+    conceded: u8
+) {
+    let status = scores.entry(team_name).or_insert(Team{
+        goals_scored: 0,
+        goals_conceded: 0
+    });
+    // 只有第一次发现没有这个队伍的时候才会进or_insert
+    //统一初始化为0,后面统一加
+    //let status 只是为了拿到所有权
+    
+    status.goals_scored += scored;
+    status.goals_conceded += conceded;
+}
+
 
 #[cfg(test)]
 mod tests {
