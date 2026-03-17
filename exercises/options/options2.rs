ec4c4da4eea89c623c78b2aaa8250e8f4d65c26a
E_Power <energy_power233@qq.com>
2026-03-17 23:17:15 +0800

    50-

--- exercises/options/options2.rs
+++ exercises/options/options2.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint options2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
@@ -13,7 +11,7 @@ mod tests {
         let optional_target = Some(target);
 
         // TODO: Make this an if let statement whose value is "Some" type
-        word = optional_target {
+        if let Some(word) = optional_target {
             assert_eq!(word, target);
         }
     }
@@ -32,7 +30,10 @@ mod tests {
         // TODO: make this a while let statement - remember that vector.pop also
         // adds another layer of Option<T>. You can stack `Option<T>`s into
         // while let and if let.
-        integer = optional_integers.pop() {
+        while let Some(Some(integer)) = optional_integers.pop() {
+            //optional_integers 是 Vec<Option<i8>>
+            /* .pop返回一个Option枚举,如果它成功弹出一个元素,那么返回Some(元素),否则返回None
+            如此这般, optional_integers.pop 的类型应是Option<Option<i8>>*/
             assert_eq!(integer, cursor);
             cursor -= 1;
         }
--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 struct Point {
     x: i32,
     y: i32,
@@ -12,9 +10,11 @@ struct Point {
 
 fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
-
+    //Some(Point) or None
     match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        //if this success, y's ownership would move to p
+        //and p will be deleted when this frame done
         _ => panic!("no match!"),
     }
     y; // Fix without deleting this line.
